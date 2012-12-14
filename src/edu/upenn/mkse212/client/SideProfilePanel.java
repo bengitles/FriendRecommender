3edd7be2ed1cf53ba0f6447cbca256bf32bf8d5b
Ben Gitles <bengitles@gmail.com>
2012-12-14 05:24:44 -0500

    death by MKSE212

--- src/edu/upenn/mkse212/client/SideProfilePanel.java
+++ src/edu/upenn/mkse212/client/SideProfilePanel.java
@@ -85,7 +85,6 @@ public class SideProfilePanel {
 										parent.getSidePanel().hide();
 										parent.getSidePanel().display(username);
 									}
-									
 								});
 							}			
 						});
